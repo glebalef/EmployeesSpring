5ec838cfa0d46fdba2186794564acb1190b94dd3
glebalef <gleb.alef@gmail.com>
2022-06-24 14:05:34 +0300

    Домашка по спискам + Spring

---
+++ src/main/java/com/skypro/EmployeesListProject/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.skypro.EmployeesListProject;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/add")
+    public Employee addEmployee(@RequestParam(value = "firstName") String firstName,
+                                @RequestParam(value = "lastName") String lastName) {
+       return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping(path = "/print")
+    public ArrayList<Employee> printAll() {
+        return employeeService.printAll();
+    }
+
+    @GetMapping(path = "/remove")
+    public Employee removeEmployee(@RequestParam(value = "firstName") String firstName,
+                                   @RequestParam(value = "lastName") String lastName) {
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping(path = "/find")
+    public Employee findEmployee(@RequestParam(value = "firstName") String firstName,
+                                 @RequestParam(value = "lastName") String lastName) {
+        return findEmployee(firstName, lastName);
+    }
+}
+
